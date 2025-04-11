a2561659c6e48077611c645f2ba687a62da62739
chonggi-tokhu <120172454+chonggi-tokhu@users.noreply.github.com>
2025-04-11 21:56:07 +0900

    .

--- audio/FluidR3_GM/AR-7778-mp3.js
+++ audio/FluidR3_GM/AR-7778-mp3.js
@@ -8,7 +8,7 @@ MIDI.Soundfont.acoustic_grand_piano = {
     "Ds4": "https://chonggi-tokhu.github.io/AR-7778/audio/FluidR3_GM/AR-7778/Ds4.wav",
     "E4": "https://chonggi-tokhu.github.io/AR-7778/audio/FluidR3_GM/AR-7778/E4.wav",
     "F4": "https://chonggi-tokhu.github.io/AR-7778/audio/FluidR3_GM/AR-7778/F4.wav",
-    "Fs4": "https://chonggi-tokhu.github.io/AR-7778/audio/FluidR3_GM/AR-7778/Es4.wav",
+    "Fs4": "https://chonggi-tokhu.github.io/AR-7778/audio/FluidR3_GM/AR-7778/Fs4.wav",
     "G4": "https://chonggi-tokhu.github.io/AR-7778/audio/FluidR3_GM/AR-7778/G4.wav",
     "Gs4": "https://chonggi-tokhu.github.io/AR-7778/audio/FluidR3_GM/AR-7778/Gs4.wav",
     "A4": "https://chonggi-tokhu.github.io/AR-7778/audio/FluidR3_GM/AR-7778/A4.wav",
@@ -27,6 +27,4 @@ MIDI.Soundfont.acoustic_grand_piano = {
     "As5": "https://chonggi-tokhu.github.io/AR-7778/audio/FluidR3_GM/AR-7778/As5.wav",
     "B5": "https://chonggi-tokhu.github.io/AR-7778/audio/FluidR3_GM/AR-7778/B5.wav",
     "C6": "https://chonggi-tokhu.github.io/AR-7778/audio/FluidR3_GM/AR-7778/C6.wav",
-    "Cs6": "https://chonggi-tokhu.github.io/AR-7778/audio/FluidR3_GM/AR-7778/Cs6.wav",
-    "D6": "https://chonggi-tokhu.github.io/AR-7778/audio/FluidR3_GM/AR-7778/D6.wav",
 };
